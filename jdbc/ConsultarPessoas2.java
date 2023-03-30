49405943b626c3b38803ca743dab1996b585f4a0
Rodrigo Raiter Mathias <rodrigormathias@hotmail.com>
2023-03-30 15:09:59 -0300

    TodasEscolhasEExtractedMethods

--- jdbc/ConsultarPessoas2.java
+++ jdbc/ConsultarPessoas2.java
@@ -16,8 +16,8 @@ public class ConsultarPessoas2 {
 		Scanner leitor = new Scanner(System.in);
 
 		System.out
-				.println("Você possuí três opções de pesquisa de nome de usuário: " + "[1] Para iniciais. Ex.: PE(dro)"
-						+ "[2] Para terminação. Ex.:(pe)DRO" + "[3] Ambas." + "Digite o número da opção desejada:");
+				.println("Você possuí três opções de pesquisa de nome de usuário:\n" + "[1] Para iniciais. Ex.: PE(dro) \n"
+						+ "[2] Para terminação. Ex.:(pe)DRO \n" + "[3] Ambas. \n" + "Digite o número da opção desejada:");
 		int escolha = leitor.nextInt();
 		leitor.nextLine();
 		System.out.println("Por favor, agora informe as sílabas que o sistema deve procurar:");
@@ -27,17 +27,46 @@ public class ConsultarPessoas2 {
 		Connection conexao = FabricaDeImport.getConnection();
 
 		if (escolha == 1) {
-			String sql = "SELECT * FROM pessoas WHERE name LIKE ?";
-			PreparedStatement stmt= conexao.prepareStatement(sql);
+			PreparedStatement stmt = prepareString(conexao);
 			stmt.setString(1,silabas + "%");
 			ResultSet resultado = stmt.executeQuery();
 			List<Pessoa> pessoas = new ArrayList<>();
 			possiblePrints(resultado, pessoas);
 
 		}
+		else if( escolha == 2) {
+			PreparedStatement stmt = prepareString(conexao);
+			stmt.setString(1,"%" + silabas);
+			ResultSet resultado = stmt.executeQuery();
+			List<Pessoa> pessoas = new ArrayList<>();
+			possiblePrints(resultado, pessoas);
+		}
+		else if (escolha == 3) {
+			PreparedStatement stmt = prepareString(conexao);
+			stmt.setString(1,"%" + silabas + "%");
+			ResultSet resultado = stmt.executeQuery();
+			List<Pessoa> pessoas = new ArrayList<>();
+			possiblePrints(resultado, pessoas);
+		}
+		else {
+			System.out.println("Busca inválida, tente novamente.");
+		}
 
 	}
 
+
+
+	//preparar o statement
+	private static PreparedStatement prepareString(Connection conexao) throws SQLException {
+		String sql = "SELECT * FROM pessoas WHERE name LIKE ?";
+		PreparedStatement stmt= conexao.prepareStatement(sql);
+		return stmt;
+	}
+	
+	
+	
+	
+	// método para chamadas de texto
 	private static void possiblePrints(ResultSet resultado, List<Pessoa> pessoas) throws SQLException {
 		while (resultado.next()) {
 			int code = resultado.getInt("code");
